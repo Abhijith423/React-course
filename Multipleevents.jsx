1e4034235bfff1304dfba35f505966843bca2961
Abhijith <abhijithemes@gmail.com>
2025-06-27 23:53:21 +0530

    Multipleevents.jsx

--- Multipleevents.jsx
+++ Multipleevents.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+
+function Message(){
+
+  
+    return(
+      <div>
+        <form>
+          <label htmlFor="name">Name:</label>
+         <input type="text" value='jithu' />
+         <label htmlFor="age">Age:</label>
+         <input type="text" value='25' />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+}
+export default Message
 
