f1d2d6a5ab0324bcc90cfbc5570694c23ad1c483
Abhijith <abhijithemes@gmail.com>
2025-06-27 23:40:30 +0530

    form.jsx

---
+++ form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Message(){
+
+  
+    return(
+      <div>
+        <form>
+          <input type="text" />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+}
+export default Message
